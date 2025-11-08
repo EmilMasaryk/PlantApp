7b737ccfe6eaae8819cdd6cfb76899625ff960ef
EmilMasaryk <emil.masaryk.academic@gmail.com>
2025-11-08 18:10:41 +0100

    BugFix, Supabase - Season -> type must be text[] not text

--- PlantApp-WebApp/src/supabaseClient.js
+++ PlantApp-WebApp/src/supabaseClient.js
@@ -5,6 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://ypyiqrjectsarvtabuad.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlweWlxcmplY3RzYXJ2dGFidWFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MTM2MDQsImV4cCI6MjA3ODE4OTYwNH0.Vn7m9ZsNa0bMtzsnB9m0VAiOKbndc7c-E9VJN77Clw4";
+
 // ----------------------------
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
